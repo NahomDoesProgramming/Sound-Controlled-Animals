0be1b790417d114c8b77a0ff110aa4e94987b6c1
NahomDoesProgramming <84427393+NahomDoesProgramming@users.noreply.github.com>
2021-12-16 20:15:22 -0800

    Update main.js

--- main.js
+++ main.js
@@ -7,3 +7,42 @@ function modelReady()
 {
     classifier.classify(gotResults);
 }
+function gotResults(error, results)
+{
+    if(error)
+    {
+       console.error(error); 
+    }
+    else
+    {
+        console.log(results)
+        random_number_r = Math.floor(Math.random() * 255) + 1;
+        random_number_g = Math.floor(Math.random() * 255) + 1;
+        random_number_b = Math.floor(Math.random() * 255) + 1;
+            document.getElementById("result_label").innerHTML = 'I can hear ' + results[0].label.toLowerCase();
+        document.getElementById("result_confidence").innerHTML = 'Accuracy: ' + (results[0].confidence*100).toFixed(2) + "%";
+        document.getElementById("result_label").style.color = "rgb("+ random_number_r+","+ random_number_g + "," + random_number_b+")";
+        document.getElementById("result_confidence").style.color = "rgb("+ random_number_r+","+ random_number_g + "," + random_number_b+")";
+        image = document.getElementById("image");
+        if(results[0].label == "Barking")
+        {
+            image.src = 'dog.png';
+        }
+        else if(results[0].label == "Meowing")
+        {
+            image.src = 'cat.jpg';
+        }
+        else if(results[0].label == "Chicken Noises")
+        {
+            image.src = 'chicken.jpeg';
+        }
+        else if(results[0].label == "Mooing")
+        {
+            image.src = 'cow.jpeg';
+        }
+        else
+        {
+            image.src = 'audio.png';
+        }
+    }
+}
